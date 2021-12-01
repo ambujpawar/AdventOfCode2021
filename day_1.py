936fdf4a1725e3605c3b4c578630d6ae853fe678
ambuj <pawar.ambuj@gmail.com>
2021-12-01 21:33:51 +0100

    Add: Solution Day1 Part2

--- day_1.py
+++ day_1.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import List, Mapping
 
 
@@ -7,20 +8,33 @@ def ReadInput(filepath: str) -> List[str]:
     return [line.strip() for line in input_strings]
 
 
+def NumDepthIncreased(input_as_list_int: List[int]) -> int:
+    """Returns the number of times the depth has increased"""
+    count = 0
+    for index, _ in enumerate(input_as_list_int):
+        if index == 0:
+            continue
+        if input_as_list_int[index] - input_as_list_int[index-1] > 0:
+            count += 1
+    return count
+
+
+def SlidingWindowSum(input_as_list_int: List[int], window_size: int =3) -> List[int]:
+    """Returns the sliding window sum for a provided window size"""
+    numpy_array = np.array(input_as_list_int)
+    sliding_window_sum = np.convolve(numpy_array, np.ones(window_size, dtype=int), mode='valid')
+    return sliding_window_sum
+
+
 def Main():
     input_as_list_str = ReadInput('/Users/ambuj/Desktop/GithubProjects/AdventOfCode2021/data/day_1.txt')
     input_as_list_int = list(map(int, input_as_list_str))
-    count = 0
-
-    for index, num in enumerate(input_as_list_int):
-
-        if index == 0:
-            continue
-        
-        if input_as_list_int[index] - input_as_list_int[index-1] > 0:
-            count += 1
     
-    print('The number of times depth has increased is: {}'.format(count))
+    count = NumDepthIncreased(input_as_list_int)
+    count_2 = NumDepthIncreased(SlidingWindowSum(input_as_list_int))
+
+    print('The Solution for Part 1 is: {}'.format(count))
+    print('The Solution for Part 2 is: {}'.format(count_2))
 
 
 if __name__ == '__main__':
